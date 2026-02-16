3f9dd750cc64b7ededddc67ce40f2efa04c73572
Emanuel Carta <138155216+Emanueluco@users.noreply.github.com>
2026-02-15 23:01:33 -0600

    Refactor CarritoRepository to CarritoJpaRepository

---
+++ uamiShop/src/main/java/com/uamishop/ventas/repository/CarritoJpaRepository.java
@@ -0,0 +1,13 @@
+package com.uamishop.ventas.repository;
+
+import com.uamishop.shared.domain.ClienteId;
+import com.uamishop.ventas.domain.Carrito;
+import com.uamishop.ventas.domain.CarritoId;
+import com.uamishop.ventas.domain.EstadoCarrito;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface CarritoJpaRepository extends JpaRepository<Carrito, CarritoId> {
+    
+    Optional<Carrito> findByClienteIdAndEstado(ClienteId clienteId, EstadoCarrito estado);
+}
