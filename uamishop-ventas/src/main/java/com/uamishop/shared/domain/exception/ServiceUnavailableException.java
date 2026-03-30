56c0168ba2a620e853bbc4cb3a4c80cdcc4e66bc
Emanuel Carta <138155216+Emanueluco@users.noreply.github.com>
2026-03-30 03:28:50 -0600

    Añadir ServiceunavailableException

---
+++ uamishop-ventas/src/main/java/com/uamishop/shared/domain/exception/ServiceUnavailableException.java
@@ -0,0 +1,7 @@
+package com.uamishop.shared.domain.exception;
+
+public class ServiceUnavailableException extends RuntimeException {
+    public ServiceUnavailableException(String message) {
+        super(message);
+    }
+}
