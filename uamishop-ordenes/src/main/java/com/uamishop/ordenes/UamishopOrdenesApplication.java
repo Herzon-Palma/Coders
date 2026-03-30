499e204ca45ac1fbfefc0299d71345a9e362ae8d
Emanuel Carta <138155216+Emanueluco@users.noreply.github.com>
2026-03-30 03:22:13 -0600

    Habilitar la programación en UamishopOrdenesApplication

--- uamishop-ordenes/src/main/java/com/uamishop/ordenes/UamishopOrdenesApplication.java
+++ uamishop-ordenes/src/main/java/com/uamishop/ordenes/UamishopOrdenesApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication(scanBasePackages = {"com.uamishop.ordenes", "com.uamishop.shared", "com.uamishop.catalogo.api", "com.uamishop.ventas.api"})
 @EntityScan(basePackages = {"com.uamishop.ordenes", "com.uamishop.shared"})
 @EnableAsync
+@org.springframework.scheduling.annotation.EnableScheduling
 public class UamishopOrdenesApplication {
 
 	public static void main(String[] args) {
